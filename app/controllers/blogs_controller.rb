1dbbab6af60cd2a1be355c014ea3c3c3b5075de6
masaki-y1117 <my1117yamamoto@gmail.com>
2021-01-08 06:23:54 +0000

    [Finish]3-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,6 +18,13 @@ class BlogsController < ApplicationController
   end
 
   def edit
+    @blog = Blog.find(params[:id])
+  end
+  
+  def update
+    blog = Blog.find(params[:id])
+    blog.save
+    redirect_to blog_path(blog)
   end
   
   private
